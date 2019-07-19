be42343996a2449a77aeb43b34963520dc58bf25
Roy Honders <royhonders@gmail.com>
2019-07-19 17:47:14 +0200

    Add test for models

---
+++ tests/models/partner.spec.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Partner = require('../../models/partner')
+
+test('Partner should be constructed correctly', () => {
+    expect(new Partner(input.name, input.address)).toEqual(expected)
+})
+
+test('Partner should have no name', () => {
+    let x
+    delete expected.name
+    expect(new Partner(x, input.address)).toEqual(expected)
+})
+
+const input = {
+    "name": "Wavetech",
+    "address": {
+        "no": "185",
+        "street": "Hagmolenbeekweg",
+        "city": "Enschede",
+        "country": "The Netherlands"
+    }
+}
+
+const expected =  {
+    "name": "Wavetech",
+    "address": {
+        "no": "185",
+        "street": "Hagmolenbeekweg",
+        "city": "Enschede",
+        "country": "The Netherlands"
+    },
+    "location": null
+}
